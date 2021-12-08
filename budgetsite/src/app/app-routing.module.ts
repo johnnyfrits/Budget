480f719e0fb316bf55f477aac52848211b9b7bc7
Johnny Frits <johnny.frits@outlook.com>
2021-12-08 13:53:22 -0400

    Aprimorado o projeto

--- budgetsite/src/app/app-routing.module.ts
+++ budgetsite/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 
 // import { HomeComponent } from "./views/home/home.component";
 import { AccountViewComponent } from './views/account-view/account-view.component';
 import { CardViewComponent } from './views/card-view/card-view.component';
 
 const routes: Routes = [
-  // {
-  //   path: "",
-  //   component: HomeComponent
-  // },
+  {
+    path: "",
+    component: AccountViewComponent
+  },
   {
     path: "accounts",
     component: AccountViewComponent
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
