77305ce693056c9066acd0d3f44a2605a7beb8ec
Johnny Frits <johnny.frits@outlook.com>
2022-01-05 21:57:07 -0400

    Aprimorado leiaute

--- budgetsite/src/app/components/accountpostings/accountpostings.component.ts
+++ budgetsite/src/app/components/accountpostings/accountpostings.component.ts
@@ -61,6 +61,8 @@ export class AccountPostingsComponent implements OnInit {
 
   getAccountTotals() {
 
+    this.hideProgress = false;
+
     this.accountService.getAccountTotals(this.accountId, this.reference).subscribe(
       {
         next: account => {
