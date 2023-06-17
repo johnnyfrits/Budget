9c371015a16a3aee69fb4753ac5fccf679355bb8
Johnny Frits <johnny.frits@outlook.com>
2023-06-17 02:46:51 -0400

    Commit made from VS Code Azure Static Web Apps

--- budgetsite/src/environments/environment.prod.ts
+++ budgetsite/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://budgetapi2-apim.azure-api.net/api/'
-  baseUrl: 'https://budgetapiapi.azure-api.net/api/'
+  // baseUrl: 'https://budgetapiapi.azure-api.net/api/'
+  baseUrl: 'https://budgetapimanagementservice.azure-api.net/api/'
 };
